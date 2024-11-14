c375526e1b0b069186e514777bb6aeac5a6cf615
Osama Baker <obaker@egtit.com>
2024-11-14 15:42:19 +0300

    fixing bugs

--- gratech-buyer/assets/js/script.js
+++ gratech-buyer/assets/js/script.js
@@ -1,33 +1,3 @@
-/*-----------------------------------------------------------------
-
-Template Name: Gratech - IT Service HTML Template
-Author:  Gramentheme
-Author URI: https://themeforest.net/user/gramentheme/portfolio
-Developer: Kawser Ahmed Roni
-Version: 1.0.0
-Description: Gratech - IT Service HTML5 Template
-
--------------------------------------------------------------------
-CSS TABLE OF CONTENTS
--------------------------------------------------------------------
-
-01. preloader
-02. header
-03. swiper slider
-04. animated text with swiper slider
-05. shop products count
-06. image src change
-07. hide & show a div
-08. isotope
-09. add class & remove class
-10. magnificPopup
-11. back to top
-12. data backgrund
-13. coundown by click
-14. remove products
-15. wow animation
-
-------------------------------------------------------------------*/
 var lang = $("html").attr("lang");
 
 const keywords = {
@@ -121,15 +91,46 @@ function getKeyword(keyword) {
     ? keywords[keyword][lang] || keywords[keyword].en
     : keyword;
 }
-var imagePath;
 
+var imagePath;
 var urlPath;
 var imageDir;
-$(window).on("load", function () {
-  var pathParts = window.location.pathname.split("/");
-  imagePath = pathParts.length > 3 ? "../assets/images/" : "assets/images/";
-  urlPath = pathParts.length > 3 ? "../" : "";
 
+function switchLanguage(lang) {
+  const currentURL = window.location.href;
+  const baseUrl = "http://127.0.0.1:5500/gratech-buyer";
+  const pathArray = currentURL.split("/");
+  const pageName = pathArray[pathArray.length - 1];
+  const isArabic = pathArray.includes("ar");
+
+  if (lang === "ar" && !isArabic) {
+    // Switch to Arabic version by adding '/ar/'
+    window.location.href = `${baseUrl}/ar/${pageName}`;
+  } else if (lang === "en" && isArabic) {
+    // Switch to English version by removing '/ar/'
+    pathArray.splice(pathArray.indexOf("ar"), 1); // Remove 'ar' from path
+    window.location.href = pathArray.join("/");
+  }
+}
+
+// Attach click events to the buttons using jQuery
+
+$(window).on("load", function () {
+  const pathParts = window.location.pathname.split("/");
+  const isArabic = pathParts.includes("ar"); // Check if the current URL includes "/ar/"
+
+  // Base URL for your site (gratech-buyer folder)
+  const baseUrl = "http://127.0.0.1:5500/gratech-buyer";
+
+  // This determines if we're working in Arabic or English
+  const urlPath = isArabic ? "/ar/" : "/";
+
+  // Dynamically adjust image paths based on location
+  const imagePath =
+    pathParts.length > 3 ? "../assets/images/" : "assets/images/";
+
+  // Toggle left/right based on language
+  let imageDirRight, imageDirLeft;
   if (lang === "ar") {
     imageDirRight = "left";
     imageDirLeft = "right";
@@ -137,57 +138,98 @@ $(window).on("load", function () {
     imageDirRight = "right";
     imageDirLeft = "left";
   }
+
+  // Prepend header with dynamic navigation
   $("body").prepend(
     `<header class="header-area">
         <div class="container header__container">
             <div class="header__main">
-                <a href="${urlPath}index.html" class="logo">
+                <a href="${baseUrl}${urlPath}index.html" class="logo">
                 <img src="${imagePath}logo/logo.png" alt="logo">
                 </a>
                 <div class="main-menu">
                     <nav>
                         <ul>
-                            <li><a href="${urlPath}about.html">${getKeyword(
+                            <li><a href="${baseUrl}${urlPath}about.html">${getKeyword(
       "about"
     )}</a></li>
                             <li>
                                 <a href="#0">${getKeyword("services")}</a>
                                 <ul class="sub-menu">
-                                    <li>
-                                        <a href="${urlPath}service-solutions.html">IT Solutions</a>
-                                    </li>
-                                    <li>
-                                        <a href="${urlPath}service.html">IT Services</a>
-                                    </li>
-                                    <li>
-                                        <a href="${urlPath}service-details.html">Service Details</a>
-                                    </li>
+                                    <li><a href="${baseUrl}${urlPath}services/service-solutions.html">IT Solutions</a></li>
+                                    <li><a href="${baseUrl}${urlPath}services/service.html">IT Services</a></li>
+                                    <li><a href="${baseUrl}${urlPath}services/service-details.html">Service Details</a></li>
                                 </ul>
                             </li>
-                            <li><a href="${urlPath}contact.html"> ${getKeyword(
+                            <li><a href="${baseUrl}${urlPath}contact.html">${getKeyword(
       "contact"
     )}</a></li>
-							 <li class="ml-20 d-none d-lg-block"><a class="search-trigger" href="#0"><svg width="17"
-                                        height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <g clip-path="url(#clip0_307_344)">
-                                            <path
-                                                d="M16.0375 14.9381L12.0784 11.0334C13.0625 9.86621 13.6554 8.36744 13.6554 6.73438C13.6554 3.02103 10.5925 0 6.82774 0C3.0629 0 0 3.02103 0 6.73438C0 10.4475 3.0629 13.4683 6.82774 13.4683C8.4834 13.4683 10.0031 12.8836 11.1865 11.913L15.1456 15.8178C15.2687 15.9393 15.4301 16 15.5915 16C15.7529 16 15.9143 15.9393 16.0375 15.8178C16.2839 15.5748 16.2839 15.181 16.0375 14.9381ZM1.26142 6.73438C1.26142 3.70705 3.75845 1.24414 6.82774 1.24414C9.89695 1.24414 12.3939 3.70705 12.3939 6.73438C12.3939 9.76146 9.89695 12.2241 6.82774 12.2241C3.75845 12.2241 1.26142 9.76146 1.26142 6.73438Z"
-                                                fill="#fff" />
-                                        </g>
-                                        <defs>
-                                            <clipPath id="clip0_307_344">
-                                                <rect width="16.2222" height="16" fill="white" />
-                                            </clipPath>
-                                        </defs>
-                                    </svg>
-                                </a></li>
+                            <li class="ml-20 d-none d-lg-block">
+                                <button id="ar-button">AR</button>
+                                <button id="en-button">EN</button>
+                            </li>
                         </ul>
                     </nav>
                 </div>
             </div>
+        </div>
     </header>`
   );
 
+  // Language toggle buttons
+  $("#ar-button").click(function () {
+    switchLanguage("ar");
+  });
+
+  $("#en-button").click(function () {
+    switchLanguage("en");
+  });
+
+  // Handle language switching
+  function switchLanguage(lang) {
+    let currentURL = window.location.href;
+    const isInArabic = currentURL.includes("/ar/");
+
+    let newURL = currentURL;
+
+    // Check if we need to add or remove '/ar/'
+    if (lang === "ar" && !isInArabic) {
+      // If switching to Arabic, add '/ar/' to the URL
+      newURL = currentURL.replace(baseUrl, baseUrl + "/ar");
+    } else if (lang === "en" && isInArabic) {
+      // If switching to English, remove '/ar/'
+      newURL = currentURL.replace("/ar", "");
+    }
+
+    // Redirect to the new language URL
+    window.location.href = newURL;
+  }
+
+  // Language toggle buttons
+  $("#ar-button").click(function () {
+    switchLanguage("ar");
+  });
+
+  $("#en-button").click(function () {
+    switchLanguage("en");
+  });
+
+  function switchLanguage(lang) {
+    const currentURL = window.location.href;
+    const isInArabic = currentURL.includes("/ar/");
+
+    let newURL = currentURL;
+
+    if (lang === "ar" && !isInArabic) {
+      // If switching to Arabic and not already in Arabic, add '/ar/'
+      newURL = currentURL.replace(baseUrl, baseUrl + "/ar");
+    } else if (lang === "en" && isInArabic) {
+      // If switching to English and currently in Arabic, remove '/ar/'
+      newURL = currentURL.replace("/ar", "");
+    }
+
+    window.location.href = newURL;
+  }
   //Footer
   $("body").append(`<footer class="footer-area secondary-bg">
         <div class="footer-images footer__shape-regular-${imageDirLeft} wow slideIn${imageDirLeft}" data-wow-delay="00ms" data-wow-duration="1500ms">
@@ -238,7 +280,7 @@ $(window).on("load", function () {
                     <h3 class="footer-title">${getKeyword("Quick-Link")}</h3>
                     <ul>
                         <li><a href="about.html"><i class="fa-regular fa-angles-${imageDirRight} me-1"></i>${getKeyword("About-Digitrend")}  </a></li>
-                        <li><a href="service.html"><i class="fa-regular fa-angles-${imageDirRight} me-1"></i> ${getKeyword("Our-Services")}</a>
+                        <li><a href="services/service.html"><i class="fa-regular fa-angles-${imageDirRight} me-1"></i> ${getKeyword("Our-Services")}</a>
                         </li>
                     </ul>
                 </div>
